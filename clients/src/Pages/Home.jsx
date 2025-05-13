330fada0cad416ee5632e68af87374543415b9d7
foram <forampaneliya1204@gmail.com>
2025-05-13 12:24:08 +0530

    add

--- clients/src/Pages/Home.jsx
+++ clients/src/Pages/Home.jsx
@@ -590,7 +590,7 @@ const MarketplaceBanner = () => {
 
     {/*  */}
 
-     <section className="trending-services-section py-20 bg-gray-100">
+     <section className="trending-services-section py-20 ">
       <div className="container mx-auto px-4">
         <div className="section-heading mb-12">
           <div className="flex items-center justify-between mb-8">
