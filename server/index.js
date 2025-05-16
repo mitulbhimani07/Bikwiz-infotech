e41f34d0b6711027acae0dca776dd1ff809bd784
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-05-16 12:38:58 +0530

    setup server file and create folder

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const db = require('./config/mongoDB');
+const PORT = process.env.PORT || 8000;
+require('dotenv').config();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
