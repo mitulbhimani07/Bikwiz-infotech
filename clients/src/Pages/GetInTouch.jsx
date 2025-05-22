376f4e8160a8b3dfa7d195c0782a6af293f7fe62
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-05-22 11:39:49 +0530

    create benner section

--- clients/src/Pages/GetInTouch.jsx
+++ clients/src/Pages/GetInTouch.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import Header from '../header-footer/Header'
-import Footer from '../header-footer/Footer'
+import React from 'react';
+import Header from '../header-footer/Header';
+import Footer from '../header-footer/Footer';
 
 export default function GetInTouch() {
   return (
     <>
-      <Header/>
+      <Header />
+      <section className="relative h-[400px] w-[95%] mx-auto my-10 rounded-4xl flex items-center justify-center overflow-hidden bg-gradient-to-r from-orange-500 from-orange-500 to-[#000]">
+        {/* Left abstract shape */}
+        <div className="absolute -left-24 top-1/2 transform -translate-y-1/2 w-96 h-96 bg-gradient-to-tr from-yellow-400 via-pink-500 from-orange-500 opacity-70 blur-3xl rounded-full rotate-45 z-0"></div>
 
+        {/* Right abstract shape */}
+        <div className="absolute -right-20 top-1/3 w-96 h-96 bg-gradient-to-br from-orange-500 from-orange-500 from-orange-500 opacity-60 blur-3xl rounded-full z-0"></div>
 
-      <Footer/>
+        {/* Content on top */}
+        <div className="relative z-10 bg-white/10 backdrop-blur-md p-10 rounded-2xl border border-white/20 text-white text-center shadow-lg">
+          <h1 className="text-4xl font-bold">Get In Touch</h1>
+        </div>
+      </section>
+      <Footer />
     </>
-  )
+  );
 }
