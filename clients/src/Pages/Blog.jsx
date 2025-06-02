ebabc6f04143f5d8a29828b9a183ca9408cc2a0f
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-06-02 14:07:37 +0530

    blog respinsive

--- clients/src/Pages/Blog.jsx
+++ clients/src/Pages/Blog.jsx
@@ -149,7 +149,7 @@ export default function Blog() {
 
       {/* Blog Grid */}
      
-      <section className="w-[80%] my-20 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <section className="w-[80%] my-20 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {blogPosts.map((post) => (
           <Link to={`/singleBlog/${post._id}`} className="no-underline text-black" key={post._id}>
           <div
