4c92bec05302c40569790ec5a59b14765088e3d9
vidhisavaliya-1710 <vidhisavaliya017@gmail.com>
2025-06-02 14:16:15 +0530

    solve

--- server/routes/Authentication/BlogRoutes.js
+++ server/routes/Authentication/BlogRoutes.js
@@ -2,15 +2,13 @@ const multer = require('multer');
 const express = require('express');
 const router = express.Router();
 const blogController = require('../../controller/Authentication/BlogController');
-var path = require('path');
-
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'Public/images');
   },
   filename: function (req, file, cb) {
-      cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, file.originalname);
   }
 });
 
