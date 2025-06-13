22cb9a7ea5cd42eb8c387318985b1c7d40461b8f
vidhisavaliya-1710 <vidhisavaliya017@gmail.com>
2025-06-13 11:15:06 +0530

    basic stureucrte

--- clients/src/Pages/freelancer/Pages/FreelancerDashboard.jsx
+++ clients/src/Pages/freelancer/Pages/FreelancerDashboard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import FreelancerSidbar from '../Navbar/FreelancerSidbar'
+import FreelancerHeader from '../Navbar/FreelancerHeader'
+import ClientFooter from '../../client/navbar/ClientFooter'
 
 function FreelancerDashboard() {
   return (
-    <div>FreelancerDashboard</div>
+    <div className="min-h-screen flex flex-col lg:flex-row bg-[#fff0e5]">
+      <div className="  lg:block lg:sticky lg:top-0 lg:left-0 lg:h-screen lg:flex-shrink-0">
+              <FreelancerSidbar />
+            </div>
+
+
+             <div className="flex flex-col flex-1 min-w-0">
+                    {/* Header */}
+                    <div className="sticky top-0 z-10">
+                      <FreelancerHeader />
+                    </div>
+
+                    <main className="flex-1 overflow-y-auto px-4 sm:px-6 py-6">
+
+                        <h1>Dashboard</h1>
+                    </main>
+                      <ClientFooter/>
+                  </div>
+    </div>
   )
 }
 
