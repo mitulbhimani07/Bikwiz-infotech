8136c994dd09ec8ebf8875837b1dd918c140a2c8
foram <forampaneliya1204@gmail.com>
2025-06-16 11:29:43 +0530

    event model add

--- server/Model/ClientDashboard/EventModel.js
+++ server/Model/ClientDashboard/EventModel.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-   
+    title: { type: String },
+    startTime: { type: String },
+    endTime: { type: String },
+    date: { type: Date },
+    location: { type: String },
+    guest: [{ type: String }],
+    category: { type: String },
+    color: { type: String },
+    clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
+    notifications: {
+        twoDaysBefore: { type: Boolean, default: true },
+        dayOf: { type: Boolean, default: true },
+        oneHourBefore: { type: Boolean, default: true }
+    },
+    notified: {
+        twoDaysBefore: { type: Boolean, default: false },
+        dayOf: { type: Boolean, default: false },
+        oneHourBefore: { type: Boolean, default: false }
+    }
+}, { timestamps: true });
 
-})
+module.exports = mongoose.model('Event', eventSchema);
+
+
